bd48920718eb235a1206b653e0caa76c6f89144d
Brandon Stafford <brandon@pingswept.org>
2016-01-19 12:39:35 -0500

    Update SD test to work with rev 1.0 shield on Due

--- test/test-sd-logging/test-sd-logging.ino
+++ test/test-sd-logging/test-sd-logging.ino
@@ -2,23 +2,25 @@
 #include <SD.h>
 
 // Set the pins used
-#define chipSelect 9
-#define ledPin 13
+#define SDCS 44
+#define SDCLK_AND_LED 50
+#define SDMISO 48
+#define SDMOSI 46
 
 File logfile;
 
 void setup() {
   Serial.begin(115200);
   Serial.println("\r\nTesting SD card logging");
-  pinMode(ledPin, OUTPUT);
+  pinMode(SDCLK_AND_LED, OUTPUT);
  
   // make sure that the default chip select pin is set to
   // output, even if you don't use it:
   //pinMode(10, OUTPUT);
-  pinMode(chipSelect, OUTPUT);
+  pinMode(SDCS, OUTPUT);
 
   // see if the card is present and can be initialized:
-  if (!SD.begin(chipSelect, 11, 12, 13)) {
+  if (!SD.begin(SDCS, SDMOSI, SDMISO, SDCLK_AND_LED)) {
     Serial.println("Card init. failed!");
   } else {
     Serial.println("Card successfully initialized.");
@@ -55,5 +57,3 @@ void loop() {
   }
   delay(5000);
 }
-
-/* End code */
