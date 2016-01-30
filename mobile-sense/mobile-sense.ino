0569d64a658a41da775bd25aacb8110e04f179f1
Cosmo Borsky <me@cosmoborsky.com>
2016-01-29 20:57:19 -0500

    Rewrite-ish mobile-sense.ino
    more bettah flow
    some starter debug information
    comments?!

--- mobile-sense/mobile-sense.ino
+++ mobile-sense/mobile-sense.ino
@@ -1,162 +1,211 @@
-#include <Adafruit_GPS.h>
-#include <SD.h> // USE ADAFRUIT'S SD LIBRARY: https://github.com/adafruit/SD.git
-#include <SPI.h>
+/* * * * * * * * * * * * * * * * * * * * * * * *
+*      Mobile Sensor Platform Arduino DUE      *
+*   Comments coming soon to a commit near you  *
+*                                              *
+* * * * * * * * * * * * * * * * * * * * * * * */
+
+/* Include Libraries */
 #include <DueTimer.h>
+#include <SD.h>
+#include <SPI.h>
+#include <Adafruit_GPS.h>
 
-#define CS0        10
-#define CS1        4
-#define CS2        52
-#define SD_CARD_CS 9
-
+/* Program Settings */
 #define UPDATE_FREQUENCY 1 // baby don't hertz me
 
-#define TIMESTAMP_LENGTH   20
-#define LOCATION_LENGTH    19
-#define TEMPERATURE_LENGTH 24
+/*
+ * Here ye'! Proceed with caution!
+ */
 
-// For hardware serial 1 (recommended):
-//   GPS TX to Arduino Due Serial1 RX pin 19
-//   GPS RX to Arduino Due Serial1 TX pin 18
-#define GPSPort Serial1
-#define GPSECHO  false
-// Set GPSECHO to 'false' to turn off echoing the GPS data to the Serial console
-// Set to 'true' if you want to debug and listen to the raw GPS sentences. 
 
-/* global variables */
+/* Miscellaneous Definitions and Functions */
+#define debugPort Serial
+void debugPrint(char *txt, ...) {
+  char buf[128];
+  sprintf(buf, "%lu\t| %s", (unsigned uint32_t) millis(), txt);
+  debugPort.println(buf);
+}
 
-volatile boolean timerState = false;
-boolean timerPrevState = false;
+char* byte_to_binary( byte x )
+{
+    static char b[8+1] = {0};
+    int y;
+    long long z;
+    for (z=1LL<<7,y=0; z>0; z>>=1,y++)
+    {
+        b[y] = ( ((x & z) == z) ? '1' : '0');
+    }
 
-Adafruit_GPS GPS(&GPSPort);
+    b[y] = 0;
 
-/* end global variables */
+    return b;
+}
 
-/* functions */
+/* Timer Definitions and Functions */
+#define TIMER Timer3
+int timerLED = 13; // standard Arduino on-board LED pin
+volatile bool timerState = false;
+bool timerPrevState = false;
 
 void timerHandler(void) {
     timerState = !timerState;
-}
-
-void setupGPS(void) {
-
-    Serial.println("Initializing GPS . . .");
-
-    // 9600 NMEA is the default baud rate for Adafruit MTK GPS's- some use 4800
-    GPS.begin(9600);
-    GPSPort.begin(9600);
-
-    // uncomment this line to turn on RMC (recommended minimum) and GGA (fix data) including altitude
-    GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
-    // uncomment this line to turn on only the "minimum recommended" data
-    //GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCONLY);
-    // For parsing data, we don't suggest using anything but either RMC only or RMC+GGA since
-    // the parser doesn't care about other sentences at this time
-
-    // Set the update rate
-    GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);   // 1 Hz update rate
-    // For the parsing code to work nicely and have time to sort thru the data, and
-    // print it out we don't suggest using anything higher than 1 Hz
-
-    // Request updates on antenna status, comment out to keep quiet
-    GPS.sendCommand(PGCMD_ANTENNA);
-
-    delay(1000);
-    // Ask for firmware version
-    GPSPort.println(PMTK_Q_RELEASE);
-
-    Serial.println("GPS initialized.");
-
-}
-
-void setupSDCard(void) {
-    Serial.println("Initializing SD card . . .");
-
-    pinMode(SD_CARD_CS, OUTPUT);
-
-    if (!SD.begin(44,46,48,50)) {
-        Serial.println("Card init. failed!");
-    }
-    Serial.println("Initialized SD card.");
-}
-
-void setupThermocouples(void) {
-    Serial.println("Initializing thermocouples . . .");
-
-    SPI.begin(CS0);
-    SPI.begin(CS1);
-    SPI.begin(CS2);
-
-    Serial.println("Thermocouples initialized.");
-}
-
-void setupXBee(void) {
-    Serial.println("Initializing XBee . . .");
-    Serial2.begin(9600);
-    Serial.println("XBee initialized.");
+    digitalWrite(timerLED, timerState);
 }
 
 void setupTimer(void) {
-    Timer3.attachInterrupt(timerHandler);
-    Timer3.setFrequency(UPDATE_FREQUENCY);
-    Timer3.start();
+    pinMode(timerLED, timerState);
+
+    TIMER.attachInterrupt(timerHandler);
+    TIMER.setFrequency(UPDATE_FREQUENCY);
+    TIMER.start();
 }
 
-double readThermocouple(int slaveSelectPin)
-{
-    byte data1 = SPI.transfer(slaveSelectPin, 0, SPI_CONTINUE);
-    byte data2 = SPI.transfer(slaveSelectPin, 0, SPI_CONTINUE);
-    byte data3 = SPI.transfer(slaveSelectPin, 0, SPI_CONTINUE);
-    byte data4 = SPI.transfer(slaveSelectPin, 0, SPI_LAST);
+/* Thermocouple Definitions and Function */
+#define CS0 10
+#define CS1 4
+#define CS2 52
+byte thermocoupleState = B00000000;
+
+void setupThermocouples(void){
+    SPI.begin(CS0);
+    SPI.begin(CS1);
+    SPI.begin(CS2);
+}
+
+double readThermocouple(short cspin) {
+    byte data1 = SPI.transfer(cspin, 0, SPI_CONTINUE);
+    byte data2 = SPI.transfer(cspin, 0, SPI_CONTINUE);
+    byte data3 = SPI.transfer(cspin, 0, SPI_CONTINUE);
+    byte data4 = SPI.transfer(cspin, 0, SPI_LAST);
 
     word temp1 = word(data1, data2);
     word temp2 = word(data3, data4);
 
-    bool ned = false;
-    if (temp1 &0x8000){
-        ned = true;
-    }
-
+    byte state = B00;
     if (temp1 & 0x1)
     {
-        Serial.println("Thermocouple error!");
-    if (temp2 & 0x1)
-        Serial.println("Open circuit");
-    if (temp2 & 0x2)
-        Serial.println("VCC Short");
-    if (temp2 & 0x4)
-        Serial.println("GND Short");
+        debugPrint("Thermocouple error!");
+        if (temp2 & 0x1){
+            //("Open circuit");
+            state = B01;
+        }
+        if (temp2 & 0x2){
+            //("VCC Short");
+            state = B10;
+        }
+        if (temp2 & 0x4){
+            //("GND Short");
+            state = B10;
+        }
+        state = B11;
+    }
+    if (cspin == CS0) {
+        thermocoupleState &= ~(B11);
+        thermocoupleState |= state;
+    } else if (cspin == CS1) {
+        thermocoupleState &= ~(B11 << 2);
+        thermocoupleState |= state << 2;
+    } else if (cspin == CS2) {
+        thermocoupleState &= ~(B11 << 4);
+        thermocoupleState |= state << 4;
     }
 
     temp1 &= 0x7FFC;
     temp1 >>= 2;
 
-    float fahrenheit = 32.0 + 9.0 * temp1 / (4.0 * 5.0); // the 4.0 division is a bit shift, not part of C -> F conversion
+    //double celcius = (temp1 / 4);
+    double fahrenheit = (32.0 + 9.0 * temp1) / (4.0 * 5.0);
 
     return fahrenheit;
 }
 
-/* end functions */
+/* SD-Card Definition and Functions */
+#define SDCS 44 
+#define SDCLK_AND_LED 50
+#define SDMISO 48
+#define SDMOSI 46
 
-void setup()  
-{
-    Serial.begin(115200);
+File logfile;
+boolean logging = false;
+boolean SDActive = false;
+File dataFile;
 
-    setupGPS();
-    setupSDCard();
-    setupThermocouples();
-    setupXBee();
-
-    setupTimer();
+void setupSD(){
+    pinMode(SDCLK_AND_LED, OUTPUT);
+    pinMode(SDCS, OUTPUT);
+    
+    SDActive = SD.begin(SDCS, SDMOSI, SDMISO, SDCLK_AND_LED);
+    dataFile = SD.open("datalog.txt", FILE_WRITE);
 }
 
-void loop()
-{
+/* GPS Definitions and Functions */
+#define GPSPort Serial1
+#define GPSECHO false
+
+Adafruit_GPS GPS(&GPSPort);
+
+void setupGPS(void) {
+    GPS.begin(9600);
+    GPSPort.begin(9600);
+}
+
+/* XBee Definitions and Functions */
+#define XBeePort Serial2
+void setupXBee(void) {
+    XBeePort.begin(9600);
+}
+
+/* System Status */
+/*  *  *  *  *  *  *  *  *  *  *  *  *  *
+  Status status:                    
+    SYSTEMS OK:             0x0000
+    MISSING GPS:            0x0001
+    MISSING SD:             0x0010 
+    MISSING XBEE/WIFI:      0x0100 
+    MISSING FILELOG:        0x1000  
+*  *  *  *  *  *  *  *  *  *  *  *  *  */
+byte systemState = B000000;
+
+byte systemStatus(void) {
+    byte newStatus = B000000;
+
+    if (!GPSPort)
+        systemState |= 1;
+    if (!SDActive)
+        systemState |= 1 << 1;
+    if (!XBeePort)
+        systemState |= 1 << 2;
+    if (!dataFile)
+        systemState |= 1 << 3;
+
+    return systemState;
+}
+
+
+/* Main Arduino Setup and Loop */
+void setup() {
+    debugPort.begin(9600);
+    delay(10);
+    debugPrint("Initializing Startup Sequence . . .");
+
+    setupGPS();
+    setupSD();
+    setupThermocouples();
+    setupXBee();
+    //setupESP8266();
+
+    setupTimer();
+    debugPrint("All systems are go. Please enjoy your stay.");
+}
+
+#define TIMESTAMP_LENGTH 20
+#define LOCATION_LENGTH 19
+#define STATES_LENGTH 19
+#define TEMPERATURE_LENGTH 24
+
+void loop() {
     // read data from the GPS in the 'main loop'
     char c = GPS.read();
-    // if you want to debug, this is a good time to do it!
-    if (GPSECHO) {
-        if (c) Serial.print(c);
-    }
 
     // if a sentence is received, we can check the checksum, parse it...
     if (GPS.newNMEAreceived()) {
@@ -166,44 +215,47 @@ void loop()
             return;  // we can fail to parse a sentence in which case we should just wait for another
         }
     }
-
-    // approximately every 2 seconds or so, print out the current stats
-    if (timerState != timerPrevState) { 
+    
+    if (timerState != timerPrevState){
         timerPrevState = timerState;
-
+        
+        systemStatus();
+        // structure and send data
         int timestamp_len = 0;
         int location_len = 0;
+        int states_len = 0;
         int temperature_len = 0;
         int output_len = 0;
 
         char timestamp[TIMESTAMP_LENGTH + 1];
         char location[LOCATION_LENGTH + 1];
+        char states[STATES_LENGTH + 1];
         char temperatures[TEMPERATURE_LENGTH + 1];
-        char output[100];
+        char output[156];
 
-        File dataFile = SD.open("datalog.txt", FILE_WRITE);
         if(!dataFile) {
             Serial.print("Couldn't open log file for writing.\r\n");
         }
 
-        timestamp_len = sprintf(timestamp, "20%02d-%02d-%02dT%02d:%02d:%02dZ", GPS.year, GPS.month, GPS.day, GPS.hour, GPS.minute, GPS.seconds);
+        char sysbuf[9];
+        char tcbuf[9];
+        itoa(systemState, sysbuf, 2);
+        itoa(thermocoupleState, tcbuf, 2);
+        states_len = sprintf(states, "%s | %s", sysbuf, tcbuf);
+
+        timestamp_len = sprintf(timestamp, "20%02d-%02d-%02d %02d:%02d:%02d", GPS.year, GPS.month, GPS.day, GPS.hour, GPS.minute, GPS.seconds);
         if(GPS.fix) {
-            location_len = sprintf(location, "%6.2f%c | %6.2f%c", GPS.latitude, GPS.lat, GPS.longitude, GPS.lon);
+            location_len = sprintf(location, "%6.2f%c | %6.2f%c", GPS.latitude/100, GPS.lat, GPS.longitude/100, GPS.lon);
         } else {
-            location_len = sprintf(location, "Location | unknown ");
+            location_len = sprintf(location, "0 | 0");
         }
         temperature_len = sprintf(temperatures, "%6.2f | %6.2f | %6.2f", readThermocouple(CS0), readThermocouple(CS1), readThermocouple(CS2));
 
-        if(timestamp_len == TIMESTAMP_LENGTH && location_len == LOCATION_LENGTH && temperature_len == TEMPERATURE_LENGTH) {
-            output_len = sprintf(output, "%s | %s | %s\r\n", timestamp, location, temperatures);
-        } else {
-            output_len = sprintf(output, "Something wrong with buffer lengths: %d, %d, %d\n", timestamp_len, location_len, temperature_len);
-        }
-        Serial.print(output);
+        output_len = sprintf(output, "%s | %s | %s | %s\r\n", timestamp, location, states, temperatures);
+        debugPrint(output);
         Serial2.print(output); // send to XBee
         if(dataFile) {
             dataFile.println(output);
-            dataFile.close();
-        }
+        } 
     }
 }
